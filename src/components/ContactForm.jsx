7b95f8b388599261da1dfac11ff2e961429f2bcd
fdimopoulos <fdimopoulos@hotmail.com>
2023-06-14 23:51:52 +0300

    fix margin-inline in ContactForm and About Us page

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -20,60 +20,66 @@ function ContactForm() {
     return (
         <div>
             <h2 className="form__title">Contact Form</h2>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="username">Username</label>
-                <input
-                    id="username"
-                    name="username"
-                    type="text"
-                    placeholder="Enter your username..."
-                    value={values.username}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    className={
-                        errors.username && touched.username ? "input-error" : ""
-                    }
-                />
-                {errors.username && touched.username && (
-                    <p className="error">{errors.username}</p>
-                )}
-                <label htmlFor="email">Email</label>
-                <input
-                    id="email"
-                    name="email"
-                    type="email"
-                    placeholder="Enter your email..."
-                    value={values.email}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    className={
-                        errors.email && touched.email ? "input-error" : ""
-                    }
-                />
-                {errors.email && touched.email && (
-                    <p className="error">{errors.email}</p>
-                )}
-                <label htmlFor="message">Message</label>
-                <textarea
-                    id="message"
-                    name="message"
-                    rows="3"
-                    cols="50"
-                    placeholder="Enter your message..."
-                    value={values.confirmPassword}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    className={
-                        errors.message && touched.message ? "input-error" : ""
-                    }
-                />
-                {errors.message && touched.message && (
-                    <p className="error">{errors.message}</p>
-                )}
-                <button type="submit" className="form__button">
-                    Submit
-                </button>
-            </form>
+            <div className="form__container">
+                <form onSubmit={handleSubmit}>
+                    <label htmlFor="username">Username</label>
+                    <input
+                        id="username"
+                        name="username"
+                        type="text"
+                        placeholder="Enter your username..."
+                        value={values.username}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        className={
+                            errors.username && touched.username
+                                ? "input-error"
+                                : ""
+                        }
+                    />
+                    {errors.username && touched.username && (
+                        <p className="error">{errors.username}</p>
+                    )}
+                    <label htmlFor="email">Email</label>
+                    <input
+                        id="email"
+                        name="email"
+                        type="email"
+                        placeholder="Enter your email..."
+                        value={values.email}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        className={
+                            errors.email && touched.email ? "input-error" : ""
+                        }
+                    />
+                    {errors.email && touched.email && (
+                        <p className="error">{errors.email}</p>
+                    )}
+                    <label htmlFor="message">Message</label>
+                    <textarea
+                        id="message"
+                        name="message"
+                        rows="3"
+                        cols="50"
+                        placeholder="Enter your message..."
+                        value={values.confirmPassword}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        className={
+                            errors.message && touched.message
+                                ? "input-error"
+                                : ""
+                        }
+                    />
+                    {errors.message && touched.message && (
+                        <p className="error">{errors.message}</p>
+                    )}
+                    <button type="submit" className="form__button">
+                        Submit
+                    </button>
+                </form>
+            </div>
         </div>
     );
 }
