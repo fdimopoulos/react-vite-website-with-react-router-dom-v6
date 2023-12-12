d8b0e2f4d88bd756eedb499bb7f6566b1a0f0de3
fdimopoulos <fdimopoulos@hotmail.com>
2023-12-12 15:15:48 +0200

    convert the static values of the services page to dynamic

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -2,8 +2,11 @@ import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import { FaCheck, FaTimes } from "react-icons/fa";
 import "../App.css";
+import plans_data from "../db.json";
 
 const Services = () => {
+    const plans = plans_data.plans;
+
     return (
         <>
             <Navbar />
@@ -12,96 +15,62 @@ const Services = () => {
                     Pick the plan that's right for you
                 </h2>
                 <div class="card__container">
-                    <div class="card">
-                        <h3 class="card__title">Basic</h3>
-                        <p class="card__price">1.499€</p>
-                        <div class="card__separator"></div>
-                        <div class="card__details">
-                            <FaCheck className="check" />
-                            <p>Lorem ipsum dolor sit amet.</p>
+                    {plans.map((plan) => (
+                        <div class="card" key={plan.id}>
+                            <h3 class="card__title">{plan.title}</h3>
+                            <p class="card__price">{plan.price}€</p>
+                            <div class="card__separator"></div>
+                            <div class="card__details">
+                                {plan.description_1.checked ? (
+                                    <FaCheck className="check" />
+                                ) : (
+                                    <FaTimes className="times" />
+                                )}
+                                <p>{plan.description_1.description_title}</p>
+                            </div>
+                            <div class="card__details">
+                                {plan.description_2.checked ? (
+                                    <FaCheck className="check" />
+                                ) : (
+                                    <FaTimes className="times" />
+                                )}
+                                <p>{plan.description_2.description_title}</p>
+                            </div>
+                            <div class="card__details">
+                                {plan.description_3.checked ? (
+                                    <FaCheck className="check" />
+                                ) : (
+                                    <FaTimes className="times" />
+                                )}
+                                <p>{plan.description_3.description_title}</p>
+                            </div>
+                            <div class="card__details">
+                                {plan.description_4.checked ? (
+                                    <FaCheck className="check" />
+                                ) : (
+                                    <FaTimes className="times" />
+                                )}
+                                <p>{plan.description_4.description_title}</p>
+                            </div>
+                            <div class="card__details">
+                                {plan.description_5.checked ? (
+                                    <FaCheck className="check" />
+                                ) : (
+                                    <FaTimes className="times" />
+                                )}
+                                <p>{plan.description_5.description_title}</p>
+                            </div>
+                            <div class="card__details">
+                                {plan.description_6.checked ? (
+                                    <FaCheck className="check" />
+                                ) : (
+                                    <FaTimes className="times" />
+                                )}
+                                <p>{plan.description_6.description_title}</p>
+                            </div>
+                            <button class="card__button">Buy Now</button>
                         </div>
-                        <div class="card__details">
-                            <FaCheck className="check" />
-                            <p>Lorem ipsum dolor sit amet.</p>
-                        </div>
-                        <div class="card__details">
-                            <FaTimes className="times" />
-                            <p>Lorem ipsum dolor sit amet.</p>
-                        </div>
-                        <div class="card__details">
-                            <FaTimes className="times" />
-                            <p>Lorem ipsum dolor sit amet.</p>
-                        </div>
-                        <div class="card__details">
-                            <FaTimes className="times" />
-                            <p>Lorem ipsum dolor sit amet.</p>
-                        </div>
-                        <div class="card__details">
-                            <FaTimes className="times" />
-                            <p>Lorem ipsum dolor sit amet.</p>
-                        </div>
-                        <button class="card__button">Buy Now</button>
-                    </div>
-                    <div class="card">
-                        <h3 class="card__title">Advanced</h3>
-                        <p class="card__price">1.999€</p>
-                        <div class="card__separator"></div>
-                        <div class="card__details">
-                            <FaCheck className="check" />
-                            <p>Lorem ipsum dolor sit amet.</p>
-                        </div>
-                        <div class="card__details">
-                            <FaCheck className="check" />
-                            <p>Lorem ipsum dolor sit amet.</p>
-                        </div>
-                        <div class="card__details">
-                            <FaCheck className="check" />
-                            <p>Lorem ipsum dolor sit amet.</p>
-                        </div>
-                        <div class="card__details">
-                            <FaCheck className="check" />
-                            <p>Lorem ipsum dolor sit amet.</p>
-                        </div>
-                        <div class="card__details">
-                            <FaTimes className="times" />
-                            <p>Lorem ipsum dolor sit amet.</p>
-                        </div>
-                        <div class="card__details">
-                            <FaTimes className="times" />
-                            <p>Lorem ipsum dolor sit amet.</p>
-                        </div>
-                        <button class="card__button">Buy Now</button>
-                    </div>
-                    <div class="card">
-                        <h3 class="card__title">Premium</h3>
-                        <p class="card__price">2.999€</p>
-                        <div class="card__separator"></div>
-                        <div class="card__details">
-                            <FaCheck className="check" />
-                            <p>Lorem ipsum dolor sit amet.</p>
-                        </div>
-                        <div class="card__details">
-                            <FaCheck className="check" />
-                            <p>Lorem ipsum dolor sit amet.</p>
-                        </div>
-                        <div class="card__details">
-                            <FaCheck className="check" />
-                            <p>Lorem ipsum dolor sit amet.</p>
-                        </div>
-                        <div class="card__details">
-                            <FaCheck className="check" />
-                            <p>Lorem ipsum dolor sit amet.</p>
-                        </div>
-                        <div class="card__details">
-                            <FaCheck className="check" />
-                            <p>Lorem ipsum dolor sit amet.</p>
-                        </div>
-                        <div class="card__details">
-                            <FaCheck className="check" />
-                            <p>Lorem ipsum dolor sit amet.</p>
-                        </div>
-                        <button class="card__button">Buy Now</button>
-                    </div>
+                    ))}
                 </div>
             </section>
             <Footer />
