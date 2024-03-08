76258a60433e4c6ddd1b6241d0f4b8cb38a7ec4e
fdimopoulos <fdimopoulos@hotmail.com>
2024-03-08 18:23:40 +0200

    set max-width (ch unit) to some paragraphs

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,8 +8,10 @@ const Hero = () => {
         <>
             <section className="hero__container">
                 <div className="hero__container-text">
-                    <h1>Welcome to Iceland</h1>
-                    <p>
+                    <h1 className="hero__container-text__title">
+                        Welcome to Iceland
+                    </h1>
+                    <p className="hero__container-text__description">
                         Lorem ipsum dolor sit amet consectetur adipisicing elit.
                         Corporis dolorum iusto, eveniet nostrum eligendi modi
                         nulla porro! Laboriosam, sint? Libero animi eaque
