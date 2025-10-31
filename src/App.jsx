697f8a61d64ee9ef6f8c4a2e23b2ff22c36512c6
Your Name <your.email@example.com>
2025-10-31 22:26:24 +0200

    delete a forgotten ";" character

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
             <Hero />
             <PhotoSection />
             <Testimonials />
-            <Footer />;
+            <Footer />
             <ScrollToTopButton />
         </>
     );
