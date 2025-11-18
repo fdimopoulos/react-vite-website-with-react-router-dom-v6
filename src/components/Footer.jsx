97d8060e8ad3e6efdf39dab9d7e3c7372736f8dc
Your Name <your.email@example.com>
2025-11-18 21:50:38 +0200

    Add an aria-label to the social media links in the footer for accessibility purposes.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -18,27 +18,27 @@ const Footer = () => {
                 </p>
                 <ul className="footer__socials">
                     <li className="footer__socials--item item-1">
-                        <a href="https://www.facebook.com/" target="_blank">
+                        <a href="https://www.facebook.com/" target="_blank" aria-label="Link to Facebook page">
                             <FaFacebook />
                         </a>
                     </li>
                     <li className="footer__socials--item item-2">
-                        <a href="https://www.instagram.com/" target="_blank">
+                        <a href="https://www.instagram.com/" target="_blank" aria-label="Link to Instagram page">
                             <FaInstagramSquare />
                         </a>
                     </li>
                     <li className="footer__socials--item item-3">
-                        <a href="https://twitter.com/" target="_blank">
+                        <a href="https://twitter.com/" target="_blank" aria-label="Link to Twitter page">
                             <FaTwitter />
                         </a>
                     </li>
                     <li className="footer__socials--item item-4">
-                        <a href="https://www.youtube.com/" target="_blank">
+                        <a href="https://www.youtube.com/" target="_blank" aria-label="Link to YouTube page">
                             <FaYoutube />
                         </a>
                     </li>
                     <li className="footer__socials--item item-5">
-                        <a href="https://www.linkedin.com/" target="_blank">
+                        <a href="https://www.linkedin.com/" target="_blank" aria-label="Link to LinkedIn page">
                             <FaLinkedin />
                         </a>
                     </li>
